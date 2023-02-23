f1c872c5986ba938c0ce54b394a2c83db93c3ddb
Vincent Ng <gvinto@gmail.com>
2023-02-23 23:58:27 +0800

    generate items_db.json

---
+++ gen_items_db.py
@@ -0,0 +1,23 @@
+import glob
+import json
+
+item_list = []
+
+for filepath in glob.glob("img/*.png"):
+    
+    filename = filepath.split('\\')[1]
+    item_id = filename.split('.')[0]
+
+    item_type = item_id.split('_')[0]
+
+    # print(item_id, item_type, filename)
+
+    item_list.append({'item_id':item_id, 'item_type':item_type, 'item_path':'/img/avatar/'+filename})
+
+print(item_list)
+
+json_object = json.dumps(item_list, indent=4)
+ 
+# Writing to sample.json
+with open("items_db.json", "w") as outfile:
+    outfile.write(json_object)
