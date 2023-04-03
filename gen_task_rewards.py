5b3f451fc106b09eee59e7c8d0238b02677b9f71
Vincent Ng <gvinto@gmail.com>
2023-04-03 13:47:57 +0800

    added agency field and MOM taskboard

--- gen_task_rewards.py
+++ gen_task_rewards.py
@@ -7,6 +7,8 @@ COL_ITEMS = 4
 COL_POINTS = 7
 COL_LIMIT = 8
 
+db_items = []
+
 def clean(text:str):
     return text.replace("â€‹","").strip()
 
@@ -16,26 +18,16 @@ def all_color_variations(item_id:str) -> list:
     variation_items = [pathlib.PurePath(file).stem for file in variation_files]
     return variation_items
 
-def main():
-    # Item DB to verify starter item_ids are valid
-    db_items = []
-    with open("items_db.json", "r") as infile:
-        items_db = json.load(infile)
-
-    for item in items_db:
-        db_items.append(item['item_id'])
-
-    # print(f"{db_items=}")
-
-    task_rewards = []
-    invalid_items = []
+def read_taskboard_csv(filename:str, agency:str = 'DSAID'):
+    agency_task_rewards = []
+    agency_invalid_items = []
 
     # Extract all starter item_ids
-    with open("Taskboard_withWearables.csv", "r") as csvfile:
+    with open(filename, "r") as csvfile:
         csv_reader = csv.reader(csvfile)
         for row in csv_reader:
             # print(clean(row[COL_INITIATIVE]),clean(row[COL_TASK]),clean(row[COL_ITEMS]),clean(row[COL_POINTS]),clean(row[COL_LIMIT]))
-            if "Initiative" in row[COL_INITIATIVE] or "Adhoc" in row[COL_INITIATIVE] or row[COL_INITIATIVE].startswith("â€‹") or "Varies" in row[COL_POINTS]:
+            if not row[COL_CODE] or "Code" in row[COL_CODE]: #"Initiative" in row[COL_INITIATIVE] or "Adhoc" in row[COL_INITIATIVE] or row[COL_INITIATIVE].startswith("â€‹") or "Varies" in row[COL_POINTS]:
                 continue
 
             #extract list of items
@@ -51,10 +43,11 @@ def main():
                 if not item:
                     item_ids.remove(item)
                 elif item not in db_items:
-                    invalid_items.append(item)
+                    agency_invalid_items.append(item)
                     item_ids.remove(item)
 
             task = {
+                    "agency": agency,
                     "code": clean(row[COL_CODE]),
                     "category": clean(row[COL_INITIATIVE]),
                     "task": clean(row[COL_TASK]),
@@ -65,7 +58,32 @@ def main():
                     "items": item_ids
                 }
             
-            task_rewards.append(task)
+            agency_task_rewards.append(task)
+        
+        return agency_task_rewards,agency_invalid_items
+
+def main():
+    # Item DB to verify starter item_ids are valid
+    with open("items_db.json", "r") as infile:
+        items_db = json.load(infile)
+
+    for item in items_db:
+        db_items.append(item['item_id'])
+
+    # print(f"{db_items=}")
+
+    task_rewards = []
+    invalid_items = []
+
+    #DSAID general tasks
+    general_tasks, general_invalid_items = read_taskboard_csv("Taskboard_withWearables.csv")
+    task_rewards.extend(general_tasks)
+    invalid_items.extend(general_invalid_items)
+
+    #MOM tasks
+    mom_tasks, mom_invalid_items = read_taskboard_csv("MOM_Taskboard.csv",'MOM')
+    task_rewards.extend(mom_tasks)
+    invalid_items.extend(mom_invalid_items)
 
     # print(task_rewards)
     task_rewards_json_object = json.dumps(task_rewards, indent=4)
